7668083fb180e83ef74054dc92b5a628ee0af58b
Sayamindu Dasgupta <sayamindu@unc.edu>
2020-07-16 16:58:38 -0400

    Add code coverage plugin for Cypress
    Also changed the browser for tests to Chrome due to
    https://github.com/cypress-io/cypress/issues/2118

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -2,5 +2,6 @@ const dotenvPlugin = require("cypress-dotenv");
 
 module.exports = (on, config) => {
   config = dotenvPlugin(config, {}, true);
+  require("@cypress/code-coverage/task")(on, config);
   return config;
 };
